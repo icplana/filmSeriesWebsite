c70852e3697256d9a13a9ffe2d50a1958c8bd10f
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-07-21 17:27:50 +0200

    router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Route, Routes } from "react-router";
 import { NavBar } from "./components/NavBar";
+import { HomeLanding } from "./pages/HomeLanding";
 
 
 export function App() {
@@ -7,7 +9,9 @@ export function App() {
   return (
     <div className=" bg-gradient-to-br from-sky-800 to-sky-600 w-screen min-h-screen">
       <NavBar />
-      <h1 className="text-center mt-5 text-5xl">Start!</h1>
+      <Routes>
+        <Route path="/" element={ <HomeLanding /> } />
+      </Routes>
     </div>
   )
 }
