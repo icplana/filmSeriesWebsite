7d6657e066c33f293993e2b91028ada714de0af4
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-07-24 14:50:07 +0200

    searchbar optimized

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { useContext, useRef } from "react"
 import { searchTypes } from "../helpers/APIdata"
 import { LimitOffsetContext } from "../contexts/limit-offset/LimitOffsetContext"
 import { AllNavbar } from "./AllNavbar"
+import { SearchNavbar } from "./SearchNavbar"
 
 
 export const NavBar = () => {
@@ -35,15 +36,18 @@ export const NavBar = () => {
 
 
       <div onMouseEnter={ showSearch } onMouseLeave={ hideSearch }>
-        <Link to="/search">Search</Link>
-        <ul ref={ search } className="absolute hidden bg-gradient-to-b from-sky-800 to-sky-600 px-2 py-1 rounded-sm">
+        <span>Search</span>
+        <div ref={ search } className="hidden">
+        {/* <ul ref={ search } className="absolute hidden bg-gradient-to-b from-sky-800 to-sky-600 px-2 py-1 rounded-sm">
           <li><NavLink to="/search/comic" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Comics</NavLink></li>
           <li><NavLink to="/search/character" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Characters</NavLink></li>
           <li><NavLink to="/search/creator" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Creators</NavLink></li>
           <li><NavLink to="/search/event" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Events</NavLink></li>
           <li><NavLink to="/search/serie" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Series</NavLink></li>
           <li><NavLink to="/search/story" className={({isActive})=> isActive ?'font-semibold' :'' + "hover:underline"}>Stories</NavLink></li>
-        </ul>
+        </ul> */}
+        <SearchNavbar classNames ="absolute bg-gradient-to-b from-sky-800 to-sky-600 px-2 py-1 rounded-sm" />
+        </div>
       </div>
 
 
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,17 +1,81 @@
+import { useContext, useEffect, useState } from "react"
+
+import { LimitOffsetContext } from "../../contexts/limit-offset/LimitOffsetContext"
+
+import { baseUrl, publicKey } from "../../helpers/APIdata"
+
 
 import { SearchNavbar } from "../../components/SearchNavbar"
+import { ShowPerPage } from "../../components/ShowPerPage"
+import { Loading } from "../../components/Loading"
+
+import { ComicPreview } from "../../components/Previews/ComicPreview"
+import { EventPreview } from "../../components/Previews/EventPreview"
+import { SeriePreview } from "../../components/Previews/SeriePreview"
+import { StoryPreview } from "../../components/Previews/StoryPreview"
+import { CharacterPreview } from "../../components/Previews/CharacterPreview"
+import { CreatorPreview } from "../../components/Previews/CreatorPreview"
 
 
 
 
-export const Search = () => {    
+export const AllResults = ({ type, searchParam }) => {
+
+    const { offset, limit, loading, setLoading } = useContext( LimitOffsetContext )
+    const [data, setData] = useState([])
+
+    useEffect( () =>{ setLoading(true); setData([]) }, [] )
+
+
+    useEffect(() => {
+
+      const url = `${ baseUrl }v1/public/${ type }?limit=${ limit }&offset=${ offset }&apikey=${ publicKey }&${ searchParam }=${ input.current.value }`
+    
+      fetch( url )
+        .then( resp => resp.json() )
+        .then( data => {
+            setData(data.data);
+            setLoading(false)
+        })
+   }, [ offset, limit, type ])
+    
    
-   
+  return (      
+      <div>
+        <h2>Search</h2>
+        <SearchNavbar classNames="flex gap-1 justify-center" />
 
-  return (
-    <div>
-        <p>Buscador</p>    
-        <SearchNavbar />         
+        <div>
+
+
+            {
+                loading
+                ? ( <Loading/> )
+                : (
+                    <>
+                        <p>There is a total of { data.total + ' ' + type }. Showing { offset } to { Number(offset) + Number(limit) }.</p>
+                        <div className="flex flex-wrap gap-2 justify-center">
+                            { 
+                              type === 'comics' 
+                                ? data.results?.map( data => <ComicPreview key={ data.id } comic={ data } /> ) 
+                                : type === 'characters' 
+                                    ? data.results?.map( data => <CharacterPreview key={ data.id } character={ data } /> )
+                                    : type === 'creators'
+                                        ? data.results?.map( data => <CreatorPreview key={ data.id } creator={ data } /> )
+                                        : type === 'events'
+                                            ? data.results?.map( data => <EventPreview key={ data.id } event={ data } /> )
+                                            : type === 'series'
+                                                ? data.results?.map( data => <SeriePreview key={ data.id } serie={ data } /> )
+                                                : type === 'stories'
+                                                    ? data.results?.map( data => <StoryPreview key={ data.id } story={ data } /> )
+                                                    : ''
+                            }                
+                        </div>
+                        <ShowPerPage /> 
+                    </> 
+                )
+            }    
+       </div>
     </div>
   )
 }
