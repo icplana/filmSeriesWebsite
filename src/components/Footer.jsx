43895d50d1116d8a84db47a6e738e7349357ee18
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-07-28 17:35:45 +0200

    footer design done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <footer className="text-center">
       <hr />
       <p className="py-4">
-        Data provided by Marvel. © { date.getFullYear() } <a target="_blank" className="underline" href="https:\\marvel.com">Marvel</a>
+        Data provided by Marvel. © { date.getFullYear() } <a target="_blank" className="underlinel" href="https:\\marvel.com">Marvel</a>
       </p>
     </footer>
   )
